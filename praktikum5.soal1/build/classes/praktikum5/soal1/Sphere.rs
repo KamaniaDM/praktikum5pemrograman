3450ed03328e745289112c1cc2754666cba1475f
KamaniaDM <114984830+KamaniaDM@users.noreply.github.com>
2022-11-28 22:30:41 +0800

    Add files via upload

---
+++ praktikum5.soal1/build/classes/praktikum5/soal1/Sphere.rs
@@ -0,0 +1,1 @@
+praktikum5.soal1.Cylinder
---
+++ praktikum5.soal1/src/praktikum5/soal1/Shape.java
@@ -0,0 +1,14 @@
+//Shape
+package praktikum5.soal1;
+
+public abstract class Shape
+{
+    protected String shapeName;
+    public Shape(String name){
+        shapeName = name;
+    }
+    public abstract double area();
+    public String toString(){
+        return shapeName;
+    }
+}
